80d34ceb9366a1c96eef33e3416128e89a01d115
200433040 <71674539+200433040@users.noreply.github.com>
2020-12-07 16:14:39 -0500

    Update Lab 4.ps1

--- powershell/Lab 4.ps1
+++ powershell/Lab 4.ps1
@@ -12,37 +12,72 @@
                                                            "Space Uasge(%)" = "{0:N2}" -f (($logicaldisk.size-$logicaldisk.freespace) * 100 / $logicaldisk.size)
                                                           }}}}}
 
-"Hardware Info >>> "
+function Hdware_info {
 
 Get-WmiObject win32_computersystem | Select Model, Manufacturer | fl
+
+}
+
+function os_info {
+
+Get-WmiObject win32_operatingsystem | Select Name, Version | fl
+
+}
+
+function cpu_info{
+
+Get-WmiObject win32_processor | Select NumberOfCores, MaxClockSpeed, @{n="L1 Cache Size";e={$_.L1CacheSize -as [int]}},@{n="L2 Cache Size";e={$_.L2CacheSize -as [int]}}, @{n="L3 Cache Size";e={$_.L3CacheSize -as [int]}}| fl
+
+}
+function ram_info{
+
+Get-WmiObject win32_physicalmemory | Select Manufacture, Description,@{n= "Size(GB)" ; e={$_.Capacity / 1GB -as [int]}}, @{n="Bank";e={$_.BankLabel}} | ft -AutoSize  
+
+}
+
+function Disk_info {
+
+Get-WmiObject win32_physicalmemory | Select Manufacture, Description,@{n= "Size(GB)" ; e={$_.Capacity / 1GB -as [int]}}, @{n="Bank";e={$_.BankLabel}} | ft -AutoSize  
+
+}
+
+function net_info {
+
+get-ciminstance win32_networkadapterconfiguration | Where-Object {$_.IPEnabled -eq $True} | Select description, index, ipaddress, ipsubnet, dnsdomain, DnsServerSearchorder| Format-Table -AutoSize}
+
+
+function video_card {
+get-ciminstance win32_videocontroller | Select @{n= "Manufacturer Name" ;e = {$_.AdapterCompatibility}} , Description ,@{n= "Display Resolution "; e = {($_.CurrentHorizontalResolution.toString()) +" X " + ($_.CurrentVerticalResolution.toString())}} | fl
+}
+"Hardware Info >>> "
+
+Hdware_info 
 
 "______________________________________________________________________________________________ "
 
 "Operating System Info >>> "
 
-Get-WmiObject win32_operatingsystem | Select Name, Version | fl
+os_info
 "______________________________________________________________________________________________ "
 
 "Processor Info >>> "
 
-Get-WmiObject win32_processor | Select NumberOfCores, MaxClockSpeed, @{n="L1 Cache Size";e={$_.L1CacheSize -as [int]}},@{n="L2 Cache Size";e={$_.L2CacheSize -as [int]}}, @{n="L3 Cache Size";e={$_.L3CacheSize -as [int]}}| fl
-
+cpu_info
 "______________________________________________________________________________________________ "
 " RAM Info >>> "
+ram_info
 
-Get-WmiObject win32_physicalmemory | Select Manufacture, Description,@{n= "Size(GB)" ; e={$_.Capacity / 1GB -as [int]}}, @{n="Bank";e={$_.BankLabel}} | ft -AutoSize  
 "________________________________________________________________________________________________"
 "Disk Info >>>"
                                                          
-diskInfo | ft -AutoSize Drive,Vendor,Model,“Size(GB)”,"Free Space(GB)","Space Uasge(%)" | out-string
+Disk_info  
 
 "________________________________________________________________________________________________"
 "NetWork Adapter >>> "
 
-get-ciminstance win32_networkadapterconfiguration | Where-Object {$_.IPEnabled -eq $True} | Select description, index, ipaddress, ipsubnet, dnsdomain, DnsServerSearchorder| Format-Table -AutoSize 
-
+ net_info
 "_________________________________________________________________________________________________"
 
 "Video Card >>>"
 
-get-ciminstance win32_videocontroller | Select @{n= "Manufacturer Name" ;e = {$_.AdapterCompatibility}} , Description ,@{n= "Display Resolution "; e = {($_.CurrentHorizontalResolution.toString()) +" X " + ($_.CurrentVerticalResolution.toString())}} | fl
+video_card
